11beec2e4b899830d6f3502475b5696e8a3e0f19
Rafael-e-p <rafaelparra030@gmail.com>
2025-06-22 23:58:48 -0300

    Update bmi.c
    Adición para mostrar en que condición de la tabla se encuentra el usuario

--- bmi.c
+++ bmi.c
@@ -15,6 +15,15 @@ int main()
     
     printf("Su BMI es: %.2f\n", BMI);
     printf("Tabla de referencia de BMI:\n<18.5 | Bajo peso\n18.5 a 24.9 | Normal\n25.0 a 29.9 | Sobrepeso\n>=30| Obesidad\n");     
-   
+
+    if(BMI < 18.5){
+        printf("Su peso es bajo.");}
+        else if(BMI >= 18.5 && BMI < 25){
+            printf("Su peso es normal.");}
+            else if(BMI >= 25 && BMI < 30){
+                printf("Tiene sobrepeso.");}
+                else{
+                    printf("Tiene obesidad.");}
+    
   return 0;
 }
